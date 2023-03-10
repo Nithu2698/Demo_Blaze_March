9510fde58e22816f81de7034deaa98d1d1362391
nitesh.l <nitesh.l@QT-INBG-LT0322.QUALITEST.LOCAL>
2023-03-10 12:16:09 +0530

    test runner changed....

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -3,7 +3,8 @@ package TestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/java",
-				 glue = "StepDefinations"	)
+				 glue = "StepDefinations",
+				 plugin={"pretty","json:target/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
